e5c3cd412204937d776f880bb041c12983be7e93
zcg <zcg@woogiu.com>
2024-02-18 14:38:54 +0800

    changed

---
+++ Runtime/Test - 副本.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public static string GetTest()
+    {
+        return "123456";
+    }
+	
+	 public static string GetTest2()
+    {
+        return "123456";
+    }
+}
